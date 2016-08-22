0393b745864c37929e756cecec87ba52e6eaf5a0
rcornacchia <rlc2160@columbia.edu>
2016-08-22 08:35:39 -0400

    restructured styling

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var morgan  = require('morgan');
 var path    = require('path');
-
-
-var app = express();
+var app     = express();
+var port    = 8000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000, function() {
-    console.log('Server started on port 3000');
+app.listen(port, function() {
+    console.log('Server started on port ' + port);
 });
